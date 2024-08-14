ec4818470c16c46d41959e493202153633f86259
roylimxinwei <102473862+roylimxinwei@users.noreply.github.com>
2024-08-14 09:30:32 +0800

    models and schema

---
+++ backend/models/workoutModel.js
@@ -0,0 +1,24 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+//enforces the following schema with a timestamp
+const workoutSchema = new Schema(
+  {
+    title: {
+      type: String,
+      required: true,
+    },
+    reps: {
+      type: Number,
+      required: true,
+    },
+    load: {
+      type: Number,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+module.exports = mongoose.model("Workout", workoutSchema);
--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,8 @@
 require("dotenv").config();
 
 const express = require("express");
-const workoutRoutes = require("./routes/workout");
+const mongoose = require("mongoose");
+const workoutRoutes = require("./routes/workouts");
 
 const app = express(); // this creates an express app
 
@@ -16,7 +17,15 @@ app.use((req, res, next) => {
 // we only use routes from workout.js if URL start with /api/workouts
 app.use("/api/workouts", workoutRoutes);
 
-//listen for request
-app.listen(process.env.PORT, () => {
-  console.log("listening on port 4000!!");
-});
+//connect to db
+mongoose
+  .connect(process.env.MONGO_URI)
+  .then(() => {
+    //listen for request
+    app.listen(process.env.PORT, () => {
+      console.log("listening on port", process.env.PORT);
+    });
+  })
+  .catch((error) => {
+    console.log(error);
+  });
